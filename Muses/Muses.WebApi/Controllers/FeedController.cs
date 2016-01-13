950cf5ab13e63f5fce26fbd545627a298b26819f
isvanidze <irakli1393@gmail.com>
2016-01-13 17:19:23 +0400

    api full text search

---
+++ Muses/Muses.WebApi/Controllers/FeedController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using FdService = FeedService.Service.FeedService;
+
+namespace Muses.WebApi.Controllers
+{
+    public class FeedController : ApiController
+    {
+        private readonly FdService _feedService;
+
+        public FeedController()
+        {
+            _feedService = new FdService();
+        }
+
+        public IHttpActionResult GetNewFeeds(string searchString)
+        {
+            return Ok(_feedService.SearchFeed(searchString));
+        }
+    }
+}
